330bdc1e81f3a96f045c21f917e9e395722334cf
ivaylo-apostolov <ivaylo.apostolov@gmail.com>
2019-07-15 14:26:58 +0300

    no message

---
+++ 2.1. Inheritance/02. CSharp-OOP-Inheritance-Skeleton/Animals/Animal.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        public Animal(string name, int age, string gender)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+
+        public string Name { get; set; }
+
+        public int Age
+        {
+            get
+            {
+                return this.Age;
+            }
+            set
+            {
+                if (this.Age >= 0)
+                {
+                    this.Age = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+            }
+        }
+
+        public string Gender
+        {
+            get
+            {
+                return this.Gender;
+            }
+            set
+            {
+                if (this.Gender == "Tomcat")
+                {
+                    this.Gender = "Male";
+                }
+                else if (this.Gender == "Kitten")
+                {
+                    this.Gender = "Female";
+                }
+                if (this.Gender == "Male" || this.Gender == "Female")
+                {
+                    this.Gender = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+            }
+        }
+
+        public virtual string ProduceSound()
+        {
+            return "Animal";
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"")
+
+            return base.ToString();
+        }
+    }
+}
